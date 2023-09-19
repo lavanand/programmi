b3cc005c28d8103d03afdb60ff1d79adb85b2288
lavanand <142693591+lavanand@users.noreply.github.com>
2023-09-19 10:05:28 +0300

    Add files via upload

---
+++ Chapter 1/Ex-2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""
+Chapter-1 Exercise 2
+
+"""
+
+import sys 
+print("Python version") 
+print (sys.version) 
+print("Version info.") 
+print (sys.version_info)
